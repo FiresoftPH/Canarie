953f8bf1ce2c1344306228fd99b31188d6e2de4f
Minniesse <112859461+Minniesse@users.noreply.github.com>
2023-06-29 15:58:28 +0700

    Make change and Create Chat Page

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Login from 'src/pages/LoginPage/Login.jsx'
-import Course from 'src/pages/CoursePage/Course.jsx'
-import Term from 'src/pages/TermPage/Term.jsx'
-import './index.css'
-import { useMediaQuery } from 'react-responsive'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Login from "src/pages/LoginPage/Login.jsx";
+import Course from "src/pages/CoursePage/Course.jsx";
+import Term from "src/pages/TermPage/Term.jsx";
+import Chat from "src/pages/ChatPage/Chat.jsx";
+import "./index.css";
+import { useMediaQuery } from "react-responsive";
 
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     {/* <Login /> */}
-    <Term />
+    {/* <Term /> */}
     {/* <Course /> */}
-  </React.StrictMode>,
-)
+    <Chat />
+  </React.StrictMode>
+);
