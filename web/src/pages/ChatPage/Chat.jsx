3a8b40983e972c07ebb3041d86c7ca4969420115
Chakrit Ritthakananon <agentpenguin202@gmail.com>
2023-07-05 10:59:52 +0700

    lil changes/ started of code editor yay

--- web/src/pages/ChatPage/Chat.jsx
+++ web/src/pages/ChatPage/Chat.jsx
@@ -1,13 +1,26 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import styles from "./Chat.module.css";
 import Sidebar from "src/components/Sidebar/Sidebar";
+import CodeMirror from '@uiw/react-codemirror'
+
+import { javascript } from '@codemirror/lang-javascript'
+import { darcula } from '@uiw/codemirror-theme-darcula'
 
 function Chat() {
+const onChange = useCallback((value,viewUpdate) => {
+  console.log('value:', value);
+}, []);
+
   return (
     <div className={styles.bg_container}>
       <Sidebar/>
       <section className={styles.chat}></section>
-      <section className={styles.ide}></section>
+      <CodeMirror
+        value="console.log('helloworld!')"
+        theme={ darcula }
+        extensions={[javascript({ jsx:true})]}
+        onChange = { onChange }
+      />
       <section className={styles.output}></section>
     </div>
   );
