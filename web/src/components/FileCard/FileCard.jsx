df6d1a8b5b2ed4c434e77c13686823d33ad479c5
Minniesse <112859461+Minniesse@users.noreply.github.com>
2023-07-05 13:23:31 +0700

    Make changes

--- web/src/components/FileCard/FileCard.jsx
+++ web/src/components/FileCard/FileCard.jsx
@@ -1,15 +1,17 @@
 import EditIcon from "../../assets/EditIcon.svg";
 import styles from "./FileCard.module.css";
-import Close from '../../';
+// import Close from "../../";
 
 const FileCard = (props) => {
   return (
-    <div className={`${styles.wrapper} ${props.selected ? styles.selected : ""}`}>
+    <div
+      className={`${styles.wrapper} ${props.selected ? styles.selected : ""}`}
+    >
       {/* <> */}
-        {props.selected ? <div className={styles.backdrop} /> : ""}
-        <img className={styles.editIcon} src={EditIcon} />
-        <p>{props.name}</p>
-        
+      {props.selected ? <div className={styles.backdrop} /> : ""}
+      <img className={styles.editIcon} src={EditIcon} />
+      <p>{props.name}</p>
+
       {/* </> */}
     </div>
   );
