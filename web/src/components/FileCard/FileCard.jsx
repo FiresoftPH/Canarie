9c83a3995af2a9884864ab74e839d118146c56a5
Chakrit Ritthakananon <agentpenguin202@gmail.com>
2023-07-05 15:53:14 +0700

    lil change push1

--- web/src/components/FileCard/FileCard.jsx
+++ web/src/components/FileCard/FileCard.jsx
@@ -1,14 +1,5 @@
 import EditIcon from "../../assets/EditIcon.svg";
 import styles from "./FileCard.module.css";
-<<<<<<< HEAD
-
-const FileCard = (props) => {
-  return (
-    <div className={`${styles.wrapper} ${props.selected ? styles.selected : ""}`}>
-        {props.selected ? <div className={styles.backdrop} /> : ""}
-        <img className={styles.editIcon} src={EditIcon} />
-        <p>{props.name}</p>
-=======
 import Close from '../../assets/CloseIcon.svg';
 
 const FileCard = (props) => {
@@ -22,7 +13,6 @@ const FileCard = (props) => {
         <p>{props.name}</p>
         <img className={styles.delete} src={Close} />
       {/* </> */}
->>>>>>> 04a1e4da3c8ed786e06a3d5f28a518f56978cb36
     </div>
   );
 };
