8f4bec21e060d74c462fc2fbe76468e0a68c88f7
Minniesse <112859461+Minniesse@users.noreply.github.com>
2023-07-06 20:04:26 +0700

    Make changes

--- web/src/components/PillarCourseCard/PillarCourseCard.jsx
+++ web/src/components/PillarCourseCard/PillarCourseCard.jsx
@@ -5,8 +5,12 @@ function PillarCourseCard({ courses }) {
   return (
     <div className={styles.container_course}>
       {courses.map((course, index) => (
-        <div className={styles.text} key={index}>
-          {course}
+        <div className={styles.card} key={index}>
+          {/* <img className={styles.photo_cover} src="src/assets/DemoPic.svg" /> */}
+          <section className={styles.photo_cover}></section>
+          <section className={styles.text}>
+            <p className={styles.sub_text}>{course}</p>
+          </section>
         </div>
       ))}
     </div>
