f06340554a5180027ba6b58ed5bde614d660c6d1
Minniesse <112859461+Minniesse@users.noreply.github.com>
2023-07-06 16:06:37 +0700

    Make changes

--- web/src/components/PillarCourseCard/PillarCourseCard.jsx
+++ web/src/components/PillarCourseCard/PillarCourseCard.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Course from "../../mockDB/MOCK_DATA.json";
 
-function PillarCourseCard() {
-  return <div className="Container_Pilllar_Course"></div>;
+function PillarCourseCard({ name }) {
+  return (
+    <div>
+      <h1>{name}</h1>
+    </div>
+  );
 }
 
 export default PillarCourseCard;
