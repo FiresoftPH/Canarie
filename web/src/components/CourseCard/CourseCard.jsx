08445040af292fcb2562da581c4c01c2a72fe113
Alphago5872 <alpha.kittikhom@gmail.com>
2023-08-01 12:23:12 +0700

    Muti line input and extra features for OLD API

--- web/src/components/CourseCard/CourseCard.jsx
+++ web/src/components/CourseCard/CourseCard.jsx
@@ -1,19 +1,22 @@
-import styles from './Coursecard.module.css'
+import styles from "./Coursecard.module.css";
 import { Link } from "react-router-dom";
 
-export default function CourseCard({data}) {
+export default function CourseCard({ data }) {
   return (
     <>
-      {
-        data.map((post) => (
-          <Link to={`/Chat/${post.competency_code}/General`} className={styles.card_container} style={post.card_color}>
-            <div className={styles.competency_name}>
-              <p className={styles.text_code}>{post.competency_code}</p>
-              <p className={styles.text_pillar}>{post.pillar}</p>
-              <p className={styles.text}>{post.title}</p>
-            </div>
-          </Link>
-        ))
-      }
+      {data.map((post) => (
+        <Link
+          to={`/Chat/${post.competency_code}/General`}
+          className={styles.card_container}
+          style={{ color: post.card_color}}
+        >
+          <div className={styles.competency_name}>
+            <p className={styles.text_code}>{post.competency_code}</p>
+            <p className={styles.text_pillar}>{post.pillar}</p>
+            <p className={styles.text}>{post.title}</p>
+          </div>
+        </Link>
+      ))}
     </>
-)}
+  );
+}
